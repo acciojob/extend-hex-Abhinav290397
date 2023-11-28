a7b6aae9bf626810e2401ce4dedbf4b3c0ef1f9d
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-11-29 02:09:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+	String str = "";
+	for(let i=0;i<shortHex.length;i++){
+		if(shortHex.charAt(i) == '#')continue;
+		str += shortHex.charAt(i) + shortHex.charAt(i);
+	}
+	"#".concat(str);
+	return str;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+/*const shortHex = prompt("Enter Short Hex.");
+alert(extendHex(shortHex));*/
