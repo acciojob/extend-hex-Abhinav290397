6e92a86db226d8890d14f21cc41c2a603ce5cb7c
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-11-29 15:23:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const extendHex = (shortHex) => {
 	if(shortHex.length < 4)return false;
 	for(let i=1;i<shortHex.length;i++){
 		let ascii = shortHex.charCodeAt(i);
-         if (!(ascii >= 48 && ascii <= 57) && !(ascii >= 65 && ascii <= 70) && !(ascii >= 97 && ascii <= 102)) {
+         if (!(ascii >= 48 && ascii <= 57) && !(ascii >= 65 && ascii <= 90) && !(ascii >= 97 && ascii <= 102)) {
     return false;
 }
 		str += shortHex.charAt(i) + shortHex.charAt(i);
