f3afcfee2646b332f28c74199561243d265b6caa
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-11-29 15:21:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 const extendHex = (shortHex) => {
   // write your code here
 	String str = "";
-	for(let i=1;i<shortHex.length;i++){
-		if(shortHex.charAt(i)< 65 && shortHex.charAt(i) > 90 && shortHex.charAt(i) < 97 && shortHex.charAt(i) > 122)return false;
-	}
 	if(shortHex.length < 4)return false;
 	for(let i=1;i<shortHex.length;i++){
+		let ascii = shortHex.charCodeAt(i);
+         if (!(ascii >= 48 && ascii <= 57) && !(ascii >= 65 && ascii <= 70) && !(ascii >= 97 && ascii <= 102)) {
+    return false;
+}
 		str += shortHex.charAt(i) + shortHex.charAt(i);
 	}
 	str = "#".concat(str);
